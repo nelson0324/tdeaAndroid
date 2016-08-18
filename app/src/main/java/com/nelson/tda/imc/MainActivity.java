de40bed88b2c7120a49d7c26fca66f9eb798921d
nelson <nelson01.martinez@gmail.com>
2016-08-17 20:59:56 -0500

    Creacion de metodos y variables

--- app/src/main/java/com/nelson/tda/imc/MainActivity.java
+++ app/src/main/java/com/nelson/tda/imc/MainActivity.java
@@ -2,12 +2,30 @@ package com.nelson.tda.imc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtEstatura,txtPeso;
+    private Button btnCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtEstatura=(EditText) findViewById(R.id.txtEstatura);
+        txtPeso=(EditText) findViewById(R.id.txtPeso);
+        btnCalcular=(Button) findViewById(R.id.btnCalcular);
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    btnCalcularOnClick();
+            }
+        });
+    }
+
+    private void btnCalcularOnClick() {
     }
 }
