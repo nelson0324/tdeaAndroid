d72fda274d1ba983863ac5e5e35e5234fe87c0b9
nelson <nelson01.martinez@gmail.com>
2016-09-07 20:17:55 -0500

    Listas y menus

---
+++ PlantasMedicinales/app/src/main/java/nelson/co/com/plantasmedicinales/IMCActivity.java
@@ -0,0 +1,13 @@
+package nelson.co.com.plantasmedicinales;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IMCActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imc);
+    }
+}
---
+++ PlantasMedicinales/app/src/main/java/nelson/co/com/plantasmedicinales/MainActivity.java
@@ -0,0 +1,45 @@
+package nelson.co.com.plantasmedicinales;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView lstPlanta;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lstPlanta= (ListView) findViewById(R.id.lstPlantasMedicinales);
+        String [ ] datosLista={"Dietas","Plantas naturales", "Recetas", "IMC","OTRAS"};
+        ArrayList<String> datos= new ArrayList<String>();
+        datos.add("Dietas");
+        datos.add("Plantas naturales");
+        datos.add("Recetas");
+        datos.add("Imc");
+        datos.add("Otras");
+
+        //ArrayAdapter(Activida,estructura, datos)
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,datos);
+        lstPlanta.setAdapter(adaptador);
+        lstPlanta.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String valor= lstPlanta.getItemAtPosition(i).toString();
+                Toast.makeText(MainActivity.this, valor,Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+    }
+}
